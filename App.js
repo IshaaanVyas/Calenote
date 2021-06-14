8571e9489f85715d628270b9284df85ebf37f7b7
Ishaan Vyas <ishaan.vyas@hotmail.com>
2021-06-14 17:56:59 +0800

    Can search for modules and look for available classes

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, StyleSheet, Text, View, TextInput,FlatList } from 'react-native';
 export default function App() {
+  const [text, setText] = useState("");
+  var NUSmods_URL = `https://api.nusmods.com/v2/2020-2021/modules/${text}.json`;
+
+
+  useEffect(() => {
+    console.log("Begin to fetch data");
+    loadNUSmodsData();
+  },[text]);
+  const [something, setSomething] = useState([]);
+  async function loadNUSmodsData()  {
+    const response = await fetch(NUSmods_URL);
+    const responseData = await response?.json();
+    console.log(responseData)
+    const somethingelse = responseData?.semesterData?.filter((item) => item?.semester == 1)[0]?.timetable;
+    console.log(somethingelse)
+    setSomething(somethingelse);
+  }
+
+  function renderItem({item}) {
+    return <Text>{item.classNo}</Text>
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>NUS MODS</Text>
+      <TextInput style={styles.textBox} onChangeText={(text) => setText(text)} />
+      <FlatList style={{width: '100%'}} data={something} renderItem={renderItem} />
     </View>
   );
 }
@@ -16,6 +37,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+    height: 50,
+  },
+  textBox: {
+    borderColor: "black",
+    padding: 5,
+    backgroundColor: "yellow",
+    marginTop: 10,
+    width: "90%"
   },
 });
